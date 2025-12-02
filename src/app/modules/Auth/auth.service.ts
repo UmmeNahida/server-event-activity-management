50622976ea832a28ab88b2d4aa3d7a253d7710fc
Nahida Akther <ummenahida20@gmail.com>
2025-12-02 22:06:22 +0600

    get all user api

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,13 +2,15 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { prisma } from "../../../lib/prisma";
 import { Prisma } from "../../../../generated/prisma/client";
+import { generateToken } from "../../helper/jwtToken";
+import { envVars } from "../../config/env";
 
 
 export const registerUser = async (data: Prisma.UserCreateInput) => {
-  const { name, email, password } = data;
+  const {password,...rest } = data;
 
   // check user exists
-  const exists = await prisma.user.findUnique({ where: {email}})
+  const exists = await prisma.user.findUnique({ where: {email: rest.email!}})
   if (exists) throw new Error("User already exists");
 
   // hash password
@@ -17,16 +19,20 @@ export const registerUser = async (data: Prisma.UserCreateInput) => {
   // create user
   const user = await prisma.user.create({
     data: {
-      name,
-      email,
+      ...rest,
       password: hashedPassword,
-      role: "USER",
     },
     select: {
       id: true,
       name: true,
       email: true,
       role: true,
+      bio: true,
+      image: true,
+      interests: true,
+      hobbies: true,
+      location: true,
+      createdAt: true,
     },
   });
 
@@ -49,24 +55,18 @@ export const loginUser = async (data: Partial<Prisma.UserCreateInput>) => {
   const match = await bcrypt.compare(password, user.password);
   if (!match) throw new Error("Invalid credentials");
 
-  const token = jwt.sign(
-    {
-      id: user.id,
-      email: user.email,
-      role: user.role,
-    },
-    process.env.JWT_SECRET as string,
-    { expiresIn: "7d" }
-  );
+const payloadJwt = {
+        id: user.id,
+        email: user.email,
+        role: user.role
+    }
+    const accessToken = generateToken(payloadJwt, envVars.JWT_SECRET as string, envVars.JWT_EXPIRES_IN as string)
+    const refreshToken = generateToken(payloadJwt, envVars.JWT_REFRESH_SECRET as string, envVars.JWT_EXPIRES_IN_REFRESH as string)
 
   return {
-    token,
-    user: {
-      id: user.id,
-      email: user.email,
-      name: user.name,
-      role: user.role,
-    },
+    accessToken,
+    refreshToken,
+    user: {user},
   };
 };
 
