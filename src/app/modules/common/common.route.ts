ce014f4d19e0bc4ae61e840ad618b4d774b3e1ac
Nahida Akther <ummenahida20@gmail.com>
2026-03-19 16:39:56 +0600

    feat(Common): add optionalAuth route

--- src/app/modules/common/common.route.ts
+++ src/app/modules/common/common.route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { CommonController } from "./common.controller";
+import optionalAuth from "../../middleware/optionalAuth";
 
-const route = Router()
+const route = Router();
 
-route.get("/all-events",CommonController.getAllEvents);
+route.get("/all-events", optionalAuth, CommonController.getAllEvents);
 route.get("/top-rated", CommonController.getTopRatedEvents);
 route.get("/popular", CommonController.getPopularEvents);
-
 
 export const CommonRoute = route;
