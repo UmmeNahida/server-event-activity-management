51112f1b0cfd93c853f89d829b58797809b060c9
Nahida Akther <ummenahida20@gmail.com>
2026-03-19 16:39:21 +0600

    feat(Common): get userId from the headers

--- src/app/modules/common/common.controller.ts
+++ src/app/modules/common/common.controller.ts
@@ -1,5 +1,5 @@
-import { NextFunction, Request, Response } from "express"
-import httpStatus from "http-status"
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import { CommonService } from "./common.service";
@@ -10,7 +10,7 @@ import { eventFilterableField } from "./event.constant";
 //      const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
 //     const filter = req.query;
 //      const result = await CommonService.getAllEvents(filter);
-   
+
 //         sendResponse(res,{
 //             success: true,
 //             statusCode: httpStatus.CREATED,
@@ -19,48 +19,64 @@ import { eventFilterableField } from "./event.constant";
 //         })
 // })
 
-const getAllEvents = catchAsync(async(req:Request,res:Response, next:NextFunction)=>{
-     const options = pick(req.query, ["page", "limit", "sortBy", "sortOrder"]);
-     const fillters = pick(req.query, eventFilterableField)
+const getAllEvents = catchAsync(
+  async (
+    req: Request & { user?: any },
+    res: Response,
+    next: NextFunction,
+  ) => {
+    const options = pick(req.query, [
+      "page",
+      "limit",
+      "sortBy",
+      "sortOrder",
+    ]);
+    const fillters = pick(req.query, eventFilterableField);
+    const userId: string | null = req.user?.id ?? null;
 
-     const result = await CommonService.getAllEvents(fillters,options);
-   
-        sendResponse(res,{
-            success: true,
-            statusCode: httpStatus.CREATED,
-            message: "All Events Retrieve successfully",
-            data: result
-        })
-})
+    const result = await CommonService.getAllEvents(
+      userId,
+      fillters,
+      options,
+    );
 
-const getTopRatedEvents = catchAsync(async(req:Request,res:Response, next:NextFunction)=>{
-   
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "All Events Retrieve successfully",
+      data: result,
+    });
+  },
+);
 
-     const result = await CommonService.getTopRatedEvents();
-   
-        sendResponse(res,{
-            success: true,
-            statusCode: httpStatus.CREATED,
-            message: "Top rated Events Retrieve successfully",
-            data: result
-        })
-})
+const getTopRatedEvents = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await CommonService.getTopRatedEvents();
 
-const getPopularEvents = catchAsync(async(req:Request,res:Response, next:NextFunction)=>{
-   
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "Top rated Events Retrieve successfully",
+      data: result,
+    });
+  },
+);
 
-     const result = await CommonService.getPopularEvents();
-   
-        sendResponse(res,{
-            success: true,
-            statusCode: httpStatus.CREATED,
-            message: "Top Popular Events Retrieve successfully",
-            data: result
-        })
-})
+const getPopularEvents = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await CommonService.getPopularEvents();
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.CREATED,
+      message: "Top Popular Events Retrieve successfully",
+      data: result,
+    });
+  },
+);
 
 export const CommonController = {
   getAllEvents,
   getTopRatedEvents,
-  getPopularEvents
-}
+  getPopularEvents,
+};
